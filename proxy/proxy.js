91f18940d134cb9a83153c2a60ad74c4810a7b5f
Skyler Burwell <skyboy49707@gmail.com>
2021-11-09 18:56:39 -0800

    Comments.

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+let fs = require('fs');
 
 const app = express();
 
@@ -34,17 +35,26 @@ app.get('/pics/:url', (req, res) => {
             return res.status(500).json({ type: 'error', message: error.message });
         }
         
-        //res.contentType('application/octet-stream');
+        //res.contentType('application/octet-stream'); //Content type for binary data.
         
+        //This way is so express can't add a charset to the headers.
+        //When it comes from google it has no charset so I thought that might have been the issue but that didn't fix it.
+        //From google the content-type is 'image/jpeg'
         res.writeHeader(200, {
             'Access-Control-Allow-Origin': '*',
             'content-type': 'image/jpeg'
         });
         res.write(body);
         res.end();//*/
+        
         //res.contentType('image/jpeg');
         //res.send(body);
-    });
+    });/*.pipe(fs.createWriteStream("img.jpeg"));
+    
+    res.header('Access-Control-Allow-Origin', '*');
+    res.sendFile('img.jpeg', {root: __dirname});*/
+    //I thought maybe I could just send the file after it is saved but it doesn't work, 
+    //there is a CORS issue for some reason on the client.
 });
 
 const PORT = process.env.PORT || 3003;
