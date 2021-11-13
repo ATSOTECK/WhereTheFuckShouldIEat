aa42e46ba646923a742ee5cd198dde734bc84836
SBrown510 <Spencerb510@gmail.com>
2021-11-13 15:49:45 -0800

    fixed calls and state of cards now works
    updated cards, now they actually correctly set state

--- client/src/Components/rbl.js
+++ client/src/Components/rbl.js
@@ -13,6 +13,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import {Modal, ModalBody, ModalFooter} from "react-bootstrap";
 import ModalHeader from "react-bootstrap/ModalHeader";
+import Link from "@mui/material/Link";
 
 let lat = 38.444342620549875
 let lng = -122.7031968966762
@@ -23,12 +24,17 @@ function getLoc(res) {
     if (loc === 'undefined') {
         loc.length = 0;
     }
-    console.log("list of locations:\n", loc)
 }
 
 function pinHandler(jsonArray,ctx) {
     for(let i in jsonArray){
-        ctx.setState({pins:[...ctx.state.pins,...[{id: i,name: jsonArray[i]['name'],lat: jsonArray[i]['geometry']['location']['lat'],lng: jsonArray[i]['geometry']['location']['lng']}]]})
+        ctx.setState({pins:
+                [...ctx.state.pins,
+                    ...[{
+            id: i,
+                        name: jsonArray[i]['name'],
+                        lat: jsonArray[i]['geometry']['location']['lat']
+                        ,lng: jsonArray[i]['geometry']['location']['lng']}]]})
     }
 }
 
@@ -52,20 +58,15 @@ class Marker extends Component {
     }
 }
 
-//CREATES CARDS THAT CAN DO THINGS
-class AllCards extends Component {
-
-    state = {
-        cards: [1],
-        cardValues: [{
-            name: "There are no restaurants in your area",
-            nImg: "https://www.mountaineers.org/activities/routes-and-places/default-route-place/activities-and-routes-places-default-image/",
-            address: "No restaurants :(",
-            key: 0
-        }],
-        show: false
+class Newpop extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            show:false,
+            name: ""
+        }
     }
-
+    
     getDirections() {
 
     }
@@ -74,7 +75,91 @@ class AllCards extends Component {
         this.setState({show:!this.state.show})
         this.getDirections()
     }
+    render() {
+        return (
+            <div>
+                <Button onClick={() => {this.handleModal()}} variant="outlined" size="medium">Eat Here!</Button>
+                {/*POPUP*/}
+                <Modal show={this.state.show}>
+                    <ModalHeader>
+                        Directions:
+                    </ModalHeader>
+                    <ModalBody>
+                        {this.props.name}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={() => {this.handleModal()}} variant="outlined" size="medium">Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
 
+//COPYRIGHT just for fun
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                WTFSIE.com
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+//CREATES CARDS THAT CAN DO THINGS
+//MAKES MAP
+class SimpleMap extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            center: {
+                lat,
+                lng
+            },
+            zoom: 13,
+            pins: [{name: '', lat: 0, lng: 0, id: 999}],
+            cards: [1],
+            cardValues: [{
+                name: "There are no restaurants in your area",
+                nImg: "https://www.mountaineers.org/activities/routes-and-places/default-route-place/activities-and-routes-places-default-image/",
+                address: "No restaurants :(",
+                key: 0
+            }],
+            show: false
+        };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function(location) {
+            let newLat = {
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            };
+            lat = location.coords.latitude
+            lng = location.coords.longitude
+            this.setState({
+                center: newLat
+            })
+            console.log("Current Location\n", newLat)
+            let radius = 1999
+            let key = 'AIzaSyC-BRpx6kbf36SeESOx7IqQnri7dnkQ8ts'
+            let res;
+            fetch("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="  + location.coords.latitude  + "%2C" + location.coords.longitude + " &radius=" + radius + "&type=restaurant&keyword=cruise&key=" + key)
+                .then(response => response.json())
+                .then(data => (res = data['results']))
+                .then(data => console.log("List of Locations:\n", res))
+                .then(data => getLoc(res))
+                .then(data=> pinHandler(res,this))
+                .then(data => this.addCards())
+        }.bind(this));
+        // https://cors-anywhere.herokuapp.com/corsdemo you need to authorize this
+
+    }
 
     async addCards() {
         if (loc.length === 0) {
@@ -82,6 +167,8 @@ class AllCards extends Component {
         }
         let newCards = this.state.cards
         let newVals = this.state.cardValues
+        newVals.pop()
+        console.log(newCards.length)
         for (let i in loc) {
             newCards.push(this.state.cards.length)
             let newImg = await this.getImg(i)
@@ -92,13 +179,15 @@ class AllCards extends Component {
                 name: loc[i]["name"],
                 nImg: newImg,
                 address: loc[i]['vicinity'],
-                key: newVals.state.cardValues.key + 1
+                key: newCards.length
             })
         }
+        newCards.shift()
         this.setState({
             cards: newCards,
             cardValues: newVals
         })
+        console.log("Current State:", this.state)
     }
 
     //GETS RANDOM IMG
@@ -115,104 +204,6 @@ class AllCards extends Component {
             .then(r => r.blob())
             .then(r => (x = r))
         return x
-    }
-
-
-    render() {
-        return (
-            <main>
-                {/* Hero unit */}
-                <Container sx={{ py: 8 }} maxWidth="md">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                        {this.state.cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card
-                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                >
-                                    <CardMedia
-                                        component="img"
-                                        sx={{
-                                            // 16:9
-                                            pt: '56.25%',
-                                        }}
-                                        image={this.state.cardValues[card-1].nImg}
-                                        alt="random"
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {this.state.cardValues[card-1].name}
-                                        </Typography>
-                                        <Typography>
-                                            {this.state.cardValues[card-1].address}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Box justifyContent={"center"}>
-                                            <div>
-                                                <Button onClick={() => {this.handleModal()}} size="medium">Eat Here!</Button>
-                                                {/*POPUP*/}
-                                                <Modal show={this.state.show}>
-                                                    <ModalHeader>
-                                                        Directions:
-                                                    </ModalHeader>
-                                                    <ModalBody>
-                                                        You've chosen restaurant, {this.state.cardValues[card-1].name}
-                                                    </ModalBody>
-                                                    <ModalFooter>
-                                                        <Button onClick={() => {this.handleModal()}} variant="outlined" size="medium">Close</Button>
-                                                    </ModalFooter>
-                                                </Modal>
-                                            </div>
-                                        </Box>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </main>
-        )
-    }
-}
-
-//MAKES MAP
-class SimpleMap extends Component {
-
-
-    state = {
-        center: {
-            lat,
-            lng
-        },
-        zoom: 13,
-        pins: [{name: '',lat:0,lng:0,id:999}]
-    };
-
-    componentDidMount() {
-        navigator.geolocation.getCurrentPosition(function(location) {
-            let newLat = {
-                lat: location.coords.latitude,
-                lng: location.coords.longitude
-            };
-            lat = location.coords.latitude
-            lng = location.coords.longitude
-            this.setState({
-                center: newLat
-            })
-            console.log("Current Location\n", newLat)
-            let radius = 2999
-            let key = 'AIzaSyC-BRpx6kbf36SeESOx7IqQnri7dnkQ8ts'
-            let res;
-            fetch("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="  + location.coords.latitude  + "%2C" + location.coords.longitude + " &radius=" + radius + "&type=restaurant&keyword=cruise&key=" + key)
-                .then(response => response.json())
-                .then(data => (res = data['results']))
-                //.then(data => console.log("List of Locations:\n", res))
-                .then(data => getLoc(res))
-                .then(data=> pinHandler(res,this))
-        }.bind(this));
-        // https://cors-anywhere.herokuapp.com/corsdemo you need to authorize this
-
     }
 
     render() {
@@ -241,7 +232,47 @@ class SimpleMap extends Component {
                         color="red"
                     />
                 </GoogleMapReact>
-            <AllCards/>
+                <main>
+                    {/* Hero unit */}
+                    <Container sx={{ py: 8 }} maxWidth="md">
+                        {/* End hero unit */}
+                        <Grid container spacing={4}>
+                            {this.state.cards.map((card) => (
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card
+                                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <CardMedia
+                                            component="img"
+                                            sx={{
+                                                // 16:9
+                                                pt: '10%',
+                                            }}
+                                            style={{height: 500,
+                                                width:300}}
+                                            image={this.state.cardValues[card-1].nImg}
+                                        />
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {this.state.cardValues[card-1].name}
+                                            </Typography>
+                                            <Typography>
+                                                {this.state.cardValues[card-1].address}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Box justifyContent={"center"}>
+                                                <Newpop
+                                                    name={this.state.cardValues[card-1].name}
+                                                />
+                                            </Box>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </main>
             </div>
         );
     }
