a51012a071a82673fef626710c0de48a0841e664
Skyler Burwell <skyboy49707@gmail.com>
2021-11-15 02:58:22 -0800

    Add directions. Still needs testing.

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -4,13 +4,28 @@ let fs = require('fs');
 
 const app = express();
 
+function getDateTime() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+    let time = hours + ':' + minutes + ':' + today.getSeconds() + ' ' + ampm;
+    let dateTime = '[' + date + ' ' + time + ']';
+    
+    return dateTime;
+}
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
 app.get('/list/:url', (req, res) => {
-    console.log("list req", req.params.url);
+    console.log(getDateTime() + " list req", req.params.url);
     
     request({
         url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + req.params.url 
@@ -32,7 +47,7 @@ app.get('/list/:url', (req, res) => {
 });
 
 app.get('/pics/:url', (req, res) => {
-    console.log("pics req", req.params.url);
+    console.log(getDateTime() + " pics req", req.params.url);
     
     request({
         url: "https://maps.googleapis.com/maps/api/place/photo?photoreference=" + req.params.url 
@@ -51,5 +66,25 @@ app.get('/pics/:url', (req, res) => {
     }).pipe(res);
 });
 
+app.get('/directions/:url', (req, res) => {
+    console.log(getDateTime() + " directions req", req.params.url);
+    
+    request({
+        url: "https://maps.googleapis.com/maps/api/directions/json=" + req.params.url 
+    }, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+            if (error) {
+                console.log(error);
+            }
+            
+            if (error && error.message) {
+                return res.status(500).json({ type: 'error', message: error.message });
+            } else {
+                return res.status(500).json({ type: 'error', message: "An unknown error occured." });
+            }
+        }
+    }).pipe(res);
+});
+
 const PORT = process.env.PORT || 25565;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(getDateTime() + ` listening on ${PORT}`));
